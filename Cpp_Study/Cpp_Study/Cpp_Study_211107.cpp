89c020a3f7cc1ff81b43423b4232ffb19391784d
임재혁 <86766590+limjh1@users.noreply.github.com>
2022-01-03 22:00:36 +0900

    220103

--- Cpp_Study/Cpp_Study/Cpp_Study_211107.cpp
+++ Cpp_Study/Cpp_Study/Cpp_Study_211107.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+
+void SetHp(int hp)
+{
+	hp = 100;
+}
+
+
+
+// [매개변수][RETURN][지역변수]
 // Pointer
 int main()
 {
+	int hp = 1;
+	SetHp(hp);
+
 	// 지금까지 사용한 방식
 	// number라는 이름의 4바이트 정수 타입의 바구니를 만든다
 	// number라는 변수 스택 메모리에 할당
@@ -30,6 +42,13 @@ int main()
 	int value1 = *ptr; // ptr이라는 주소를 저장하는 바구니가 가리키는 해당 내용 가져오기
 	*ptr = 2; // 가리키는 해당 내용을 2로
 
+	// TYPE은 왜 붙여줄까?
+	// * = 포인터의 의미 = 주소를 저장하는 바구니 = 4 or 8 byte 고정 크기
+
+	// 타입의 불일치
+	__int64* ptr2 = (__int64*)&number;
+
+	*ptr2 = 0xAABBCCDDEEFF; // 16진수
 
 	return 0;
 }
